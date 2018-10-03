59c176dd405e4b6c26eeb0c58d28c102cff4cf49
lasalesi <larsericmueller@gmail.com>
2018-10-03 16:58:37 +0200

    implemented planner page

---
+++ planner/planner/page/booking_planner/booking_planner.js
@@ -0,0 +1,87 @@
+frappe.pages['booking-planner'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Booking Planner',
+		single_column: true
+	});
+    
+    frappe.booking_planner.make(page);
+    frappe.booking_planner.run(page);
+    
+    // add the application reference
+    frappe.breadcrumbs.add("Planner");
+}
+
+frappe.booking_planner = {
+    start: 0,
+    make: function(page) {
+        var me = frappe.booking_planner;
+        me.page = page;
+        me.body = $('<div></div>').appendTo(me.page.main);
+        var data = "";
+        $(frappe.render_template('booking_planner', data)).appendTo(me.body);
+        
+        // attach button handlers
+        this.page.main.find(".btn-update").on('click', function() {
+            // update view
+            frappe.booking_planner.update(page);
+        });
+    },
+    run: function(page) {
+        // prepare form data
+        var now = new Date();
+        document.getElementById("month").value = (now.getMonth() + 1);
+        document.getElementById("year").value = now.getFullYear();
+        
+        // update table
+        frappe.booking_planner.update(page);
+    },
+    update: function(page) {
+        // get form data
+        var month = parseInt(document.getElementById("month").value);
+        var year = parseInt(document.getElementById("year").value);
+        // compensate input errors
+        if (month < 1) { month = 1; document.getElementById("month").value = 1; }
+        if (month > 12) { month = 12; document.getElementById("month").value = 12; }
+        if (year < 2000) { year = 2000; document.getElementById("year").value = 2000; }
+        if (year > 2100) { year = 2100; document.getElementById("year").value = 2100; }
+        // collect booking data
+        frappe.call({
+            method: 'planner.planner.page.booking_planner.booking_planner.get_booking_overview',
+            args: {
+                'year': year,
+                'month': month
+            },
+            callback: function(r) {
+                if (r.message) {
+                    console.log(r.message);
+                    frappe.booking_planner.show_table(page, r.message);
+                } 
+            }
+        });
+    },
+    show_table: function(page, message) {
+        // display the transactions as table
+        var container = document.getElementById("table_placeholder");
+        //console.log("Container: " + container);
+        var content = frappe.render_template('booking_table', message);
+        //console.log("Content: " + content);
+        container.innerHTML = content; 
+        
+        // attach button handlers
+        /*this.page.main.find(".cell-calendar").on('click', function(d) {
+            // update view
+            frappe.booking_planner.new_booking(d);
+        });*/
+    },
+    new_booking: function(d) {
+        console.log(d.getAttribute("data-date"));
+        var now = new Date();
+		frappe.route_options = {
+			"appartment": d.getAttribute("data-appartment"),
+            "start_date": now/* d.getAttribute("data-date") */,
+            "end_date": "03-10-2018" /* d.getAttribute("data-date") */
+		}
+		frappe.new_doc("Booking");
+    }
+}
---
+++ planner/planner/page/booking_planner/booking_planner.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017-2018, libracore and contributors
+# License: AGPL v3. See LICENCE
+
+from __future__ import unicode_literals
+import frappe
+from frappe import throw, _
+from datetime import datetime
+import calendar
+
+@frappe.whitelist()
+def get_booking_overview(month=None, year=None):
+    # prepare time filter
+    now = datetime.now()
+    if not year:
+        year = now.year
+    else:
+        year = int(year)
+    if not month:
+        month = now.month
+    else:
+        month = int(month)
+    
+    # prepare month columns
+    days_per_month = calendar.monthrange(year,month)[1]
+    # find first weekday (0: Monday, ... 6: Sunday)
+    first_weekday = calendar.monthrange(year,month)[0]
+    
+    # collect headers
+    headers = []
+    weekday = first_weekday
+    for i in range(0, days_per_month):
+        headers.append({ 'day': i + 1, 'weekday': weekday})
+        weekday += 1
+        if weekday > 6:
+            weekday = 0
+    
+    # collect appartment information
+    appartments = []
+    appartment_records = frappe.get_all("Appartment", filters=None, fields=['name', 'title'])
+    for aptmt in appartment_records:
+        # find booking information
+        days = []
+        for i in range(0, days_per_month):
+            booking = frappe.get_all("Booking", 
+                filters=[
+                    ['appartment', '=', aptmt['name']], 
+                    ['start_date', '<=', '{0}-{1}-{2}'.format(year, month, (i + 1))],
+                    ['end_date', '>=', '{0}-{1}-{2}'.format(year, month, (i + 1))]
+                ], fields=['booking_type'])
+            display = "-"
+            if booking:
+                display = booking[0]['booking_type'][0]
+            days.append({ 'booking': display, 'date': '{0}-{1}-{2}'.format(year, month, (i + 1)) })
+        # add record
+        appartments.append({'title': aptmt['title'], 'days': days})
+        
+    return ( {'headers': headers, 'appartments': appartments } )
+    
+    
