f1b41bae8b45dbfdd60789c5704524385c51e5b0
Joelios <joel.mesmer@libracore.com>
2018-11-26 14:00:47 +0100

    board task 1306

--- planner/config/planner.py
+++ planner/config/planner.py
@@ -2,47 +2,41 @@ from __future__ import unicode_literals
 from frappe import _
 
 def get_data():
-    return[
-        {
-            "label": _("Planner"),
-            "icon": "octicon octicon-calendar",
-            "items": [
-                   {
-                       "type": "page",
-                       "name": "booking-planner",
-                       "label": _("Booking Planner"),
-                       "description": _("Booking Planner")
-                   }
-            ]
-        },
-        {
-            "label": _("Documents"),
-            "icon": "fa fa-bank",
-            "items": [
-                   {
-                       "type": "doctype",
-                       "name": "House",
-                       "label": _("House"),
-                       "description": _("House")
-                   },
-				   {
-                       "type": "doctype",
-                       "name": "Appartment",
-                       "label": _("Apartment"),
-                       "description": _("Apartment")
-                   },
-                   {
-                       "type": "doctype",
-                       "name": "Booking",
-                       "label": _("Booking"),
-                       "description": _("Booking")
-                   },
-                   {
-                       "type": "doctype",
-                       "name": "Cleaning",
-                       "label": _("Cleaning"),
-                       "description": _("Cleaning")
-                   }
-            ]
-        }
-    ]
+	return[
+		{
+			"label": _("Planner"),
+			"icon": "octicon octicon-calendar",
+			"items": [
+				{
+					"type": "page",
+					"name": "booking-planner",
+					"label": _("Booking Planner"),
+					"description": _("Booking Planner")
+				}
+			]
+		},
+		{
+			"label": _("Documents"),
+			"icon": "fa fa-bank",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "House",
+					"label": _("House"),
+					"description": _("House")
+				},
+				{
+					"type": "doctype",
+					"name": "Appartment",
+					"label": _("Apartment"),
+					"description": _("Apartment")
+				},
+				{
+					"type": "doctype",
+					"name": "Booking",
+					"label": _("Booking"),
+					"description": _("Booking")
+				}
+			]
+		}
+	]
