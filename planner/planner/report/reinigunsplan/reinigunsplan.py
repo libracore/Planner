51bfb78e1d5b87228f370da847be7880479d5aa4
Joelios <joel.mesmer@libracore.com>
2018-12-03 12:46:11 +0100

    board task 1395

--- planner/planner/report/reinigunsplan/reinigunsplan.py
+++ planner/planner/report/reinigunsplan/reinigunsplan.py
@@ -6,7 +6,7 @@ import frappe
 
 def execute(filters=None):
 	columns, data = [], []
-	columns = ["House:Link/House:100", "Employee:Link/Employee:100", "Name::100", "Apartment:Link/Appartment:100", "Due Date::100", "Task::100", "Default Time::100"]
+	columns = ["House:Link/House:100", "Employee:Link/Employee:100", "Name::100", "Apartment:Link/Appartment:100", "Due Date::100", "Task::100", "Default Timein min::100", "Remark::300"]
 	data = frappe.db.sql("""SELECT
 		house.`name`,
 		housecleaning.`parent`,
@@ -14,7 +14,8 @@ def execute(filters=None):
 		apartment.`name`,
 		booking.`start_date`,
 		booking.`booking_status`,
-		apartment.`time_for_cleaning`
+		apartment.`time_for_cleaning`,
+		booking.`remark`
 		FROM ((((`tabHouse` AS house
 		LEFT JOIN `tabHouse Cleaning` AS housecleaning ON house.`name` = housecleaning.`house`)
 		LEFT JOIN `tabEmployee` AS employee ON employee.`name` = housecleaning.`parent`)
@@ -22,5 +23,8 @@ def execute(filters=None):
 		LEFT JOIN `tabBooking` AS booking ON booking.`appartment` = apartment.`name`)
 		WHERE (booking.`booking_status` = 'End-Cleaning' OR booking.`booking_status` = 'Sub-Cleaning' OR booking.`booking_status` = 'Service-Cleaning')
 		AND booking.`start_date` >= '{from_date}'
-		AND booking.`start_date` <= '{to_date}'""".format(from_date=filters.from_date, to_date=filters.to_date), as_list=True)
+		AND booking.`start_date` <= '{to_date}'
+		AND housecleaning.`from` <= booking.`start_date`
+		AND housecleaning.`to` >= booking.`start_date`
+		ORDER BY booking.`start_date` ASC, house.`name` ASC, apartment.`name` ASC""".format(from_date=filters.from_date, to_date=filters.to_date), as_list=True)
 	return columns, data
