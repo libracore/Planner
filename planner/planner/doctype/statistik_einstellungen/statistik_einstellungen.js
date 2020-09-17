f1dd383b81cd0c234e92176e3ced55491ea5c343
Joelios <joel.mesmer@libracore.com>
2020-09-17 12:08:56 +0200

    statistik einstellungen

---
+++ planner/planner/doctype/statistik_einstellungen/statistik_einstellungen.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, libracore and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Statistik Einstellungen', {
+	// refresh: function(frm) {
+
+	// }
+});
