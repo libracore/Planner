a45811afd5c466d258d4a563662c18f6cadce8e0
Joelios <joel.mesmer@libracore.com>
2021-06-25 15:24:41 +0200

    verbesserungen belegungsstatistik per freitag

--- planner/planner/report/belegungsstatistik_per_freitag/belegungsstatistik_per_freitag.py
+++ planner/planner/report/belegungsstatistik_per_freitag/belegungsstatistik_per_freitag.py
@@ -7,54 +7,108 @@ from frappe.utils.data import getdate
 from datetime import date, timedelta
 
 def execute(filters=None):
-	data, columns = get_data(filters)
+    columns = get_columns(filters)
+    data = get_data(filters)
           
-	return columns, data
+    return columns, data
     
-def get_data(filters):
+def get_columns(filters):
     columns = [
         "House:Link/House:150",
-        "Total Appartments:Int"
+        "Size",
+        "Appartments:Int",
+        "Rate in %"
     ]
-    
     start_date = getdate(filters.from_date)
     end_date = getdate(filters.to_date)
     delta = timedelta(days=1)
     while start_date <= end_date:
         week_day_no = start_date.weekday()
         if week_day_no == 4:
-            columns.append("Booked on " + start_date.strftime("%d.%m.%Y") + ":Int")
+            columns.append(start_date.strftime("%d.%m.%Y") + ":Int")
         start_date += delta
+    return columns
     
+def get_data(filters):
     data = []
-    
-    houses = frappe.db.sql("""SELECT `t1`.`title` AS `house` FROM `tabHouse` AS `t1`""", as_dict=True)
+    houses = get_houses()
     for house in houses:
-        _data = []
-        _data.append(house.house)
-        sql_query = frappe.db.sql("""SELECT 
-                                        COUNT(`name`) AS `maxapp`
+        sizes = get_appartement_sizes(house.name)
+        for size in sizes:
+            _data = []
+            _data.append(house.name)
+            _data.append(size.size)
+            qty = get_appartement_qty_per_sizes(house.name, size.size)
+            _data.append(qty)
+            friday_qty = get_friday_qty(filters)
+            booked_qty = 0
+            data_to_extend = []
+            appartement_names = """SELECT
+                                        `name`
                                     FROM `tabAppartment`
-                                    WHERE `house` = '{house}'
-                                    AND (`parking` + `disabled` + `disable_statistic`) = 0
-                    """.format(house=house.house), as_dict = True)[0]
-        _data.append(sql_query.maxapp)
-                
-        start_date = getdate(filters.from_date)
-        end_date = getdate(filters.to_date)
-        delta = timedelta(days=1)
-        while start_date <= end_date:
-            week_day_no = start_date.weekday()
-            if week_day_no == 4:
-                sql_sub_query = frappe.db.sql("""SELECT 
+                                    WHERE `apartment_size` = '{size}'
+                                    AND `house` = '{house}'
+                                    AND (`parking` + `disabled` + `disable_statistic`) = 0""".format(house=house.name, size=size.size)
+            start_date = getdate(filters.from_date)
+            end_date = getdate(filters.to_date)
+            delta = timedelta(days=1)
+            while start_date <= end_date:
+                week_day_no = start_date.weekday()
+                if week_day_no == 4:
+                    sql_query = frappe.db.sql("""SELECT 
                                                     COUNT(`name`) AS `booked` 
                                                 FROM `tabBooking`
                                                 WHERE '{date}' BETWEEN `start_date` AND `end_date`
-                                                AND `house` = '{house}'
-                    """.format(date=start_date, house=house.house), as_dict = True)[0]
-                _data.append(sql_sub_query.booked)
-            start_date += delta
-        data.append(_data)
-        
+                                                AND `appartment` IN ({appartement_names})
+                                                AND `booking_status` = 'Booked'
+                        """.format(date=start_date, appartement_names=appartement_names), as_dict = True)[0]
+                    data_to_extend.append(sql_query.booked)
+                    booked_qty += sql_query.booked
+                start_date += delta
+                
+            qty_in_percent = (100 / (friday_qty * qty)) * booked_qty
+            _data.append(round(qty_in_percent, 2))
+            _data.extend(data_to_extend)
+            data.append(_data)
+    return data
     
-    return data, columns
+def get_houses():
+    sql_query = """SELECT
+                        `name`
+                    FROM `tabHouse`
+                    WHERE (`parking` + `disabled` + `disable_statistic`) = 0
+                    ORDER BY `name` ASC"""
+                    
+    return frappe.db.sql(sql_query, as_dict=True)
+    
+def get_appartement_sizes(house):
+    sql_query = """SELECT DISTINCT
+                    `apartment_size` AS `size`
+                FROM `tabAppartment`
+                WHERE (`parking` + `disabled` + `disable_statistic`) = 0
+                AND `house` = '{house}'
+                ORDER BY `apartment_size` ASC""".format(house=house)
+                
+    return frappe.db.sql(sql_query, as_dict=True)
+    
+def get_appartement_qty_per_sizes(house, size):
+    sql_query = """SELECT
+                    COUNT(`name`) AS `qty`
+                FROM `tabAppartment`
+                WHERE `apartment_size` = '{size}'
+                AND `house` = '{house}'
+                AND (`parking` + `disabled` + `disable_statistic`) = 0""".format(house=house, size=size)
+                
+    return frappe.db.sql(sql_query, as_dict=True)[0].qty
+    
+def get_friday_qty(filters):
+    qty = 0
+    start_date = getdate(filters.from_date)
+    end_date = getdate(filters.to_date)
+    delta = timedelta(days=1)
+    while start_date <= end_date:
+        week_day_no = start_date.weekday()
+        if week_day_no == 4:
+            qty += 1
+        start_date += delta
+    return qty
